43c3bebf70fafbf5538cdf6be73cb0d697a6a368
ccottrell <courage.cottrell@gmail.com>
2022-07-31 11:07:18 -0600

    added my variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,5 @@
 
+var apiKey = '044bb9c5369619c2020f969f5078b5a5';
+var cityInput = document.querySelector('#cityInput');
+var searchBtn = document.querySelector('#searchBtn');
+var searchHistoryEl = document.querySelector("#searchHistory");
