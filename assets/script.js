220f22bf7fefbd0df2fed769183c83890b359933
ccottrell <courage.cottrell@gmail.com>
2022-07-31 11:08:16 -0600

    added some javascript

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,39 @@ var apiKey = '044bb9c5369619c2020f969f5078b5a5';
 var cityInput = document.querySelector('#cityInput');
 var searchBtn = document.querySelector('#searchBtn');
 var searchHistoryEl = document.querySelector("#searchHistory");
+
+
+
+
+searchBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+  
+    if (cityInput.value === "") {
+      alert("City cannot be blank.");
+    } else {
+      document.querySelector("#cardRow").innerHTML = '';
+      document.querySelector('#todayContainer').innerHTML = '';
+      getWeather(cityInput.value);
+      save();
+      getButtons();
+    }
+  });
+  
+  keepButtons();
+  
+  
+  function save() {
+    var new_city = document.querySelector('#cityInput').value;
+    
+    if(localStorage.getItem('Cities:') == null) {
+      localStorage.setItem('Cities:', '[]');
+    }
+    var old_city = JSON.parse(localStorage.getItem('Cities:'));
+    old_city.push(new_city);
+    
+    localStorage.setItem('Cities:', JSON.stringify(old_city));
+  
+   
+    var dividerEl = document.querySelector("#divider");
+    dividerEl.className = "searchDivider";
+  }
